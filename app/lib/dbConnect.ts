9a67c761293339c665736db20680aa27ed79f5a5
Papp Zoltán <papp.zoltan@infokristaly.hu>
2023-08-10 18:49:14 +0200

    connection cache and eslintrc repaire

--- app/lib/dbConnect.ts
+++ app/lib/dbConnect.ts
@@ -1,12 +1,14 @@
 import mongoose, { ConnectOptions, connect } from "mongoose";
 
-export default async () => {
-  try {
-    if (!(mongoose.connections && mongoose.connections[0].readyState)) {
-      await connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/quiz');
-      console.log("connected to database.");
-    }
-  } catch (error) {
-    console.log("could not connect to database", error);
+let cached: undefined | Promise<void>;
+export default () => {
+  if (mongoose.connections && mongoose.connections[0].readyState)
+    return Promise.resolve();
+
+  if (!cached) {
+    cached = connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/quiz')
+      .then(() => console.log("connected to database."))
+      .catch((err) => console.log("could not connect to database", err));
   }
-};
+  return cached;
+}
